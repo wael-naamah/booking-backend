616d92835182d436be7a596096a9e884d5832e8c
Wael Naamah <w.naamah@tenderd.com>
2024-03-05 15:45:18 +0400

    system env

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,5 +1,5 @@
-import * as fs from "fs";
-import * as path from "path";
+// import * as fs from "fs";
+// import * as path from "path";
 
 export interface Env {
   env: "production" | "development";
@@ -25,25 +25,25 @@ export const getEnv = () => {
   let contents = null;
 
 
-    const envFilePath =
-      process.env.ENV_PATH || path.resolve(__dirname, "../../env/env.json");
-    contents = fs.readFileSync(envFilePath);
+    // const envFilePath =
+    //   process.env.ENV_PATH || path.resolve(__dirname, "../../env/env.json");
+    // contents = fs.readFileSync(envFilePath);
 
-  // contents = `
-  // {
-  //   "env": "${process.env.NODE_ENV}", 
-  //   "mongoUri": "${process.env.DATABASE_URI}",
-  //   "mongoUsername": "${process.env.DATABASE_USERNAME}", 
-  //   "mongoPassword": "${process.env.DATABASE_PASSWORD}", 
-  //   "database": "${process.env.DATABASE}", 
-  //   "jwt_secret_key": "${process.env.JWT_SECRET_KEY}", 
-  //   "jwt_refresh_secret_key": "${process.env.JWT_REFRESH_SECRET_KEY}",
-  //   "firebase_project_Id": "${process.env.FIREBASE_PROJECT_ID}",
-  //   "firebase_client_email": "${process.env.FIREBASE_CLIENT_EMAIL}",
-  //   "firebase_private_key": "${process.env.FIREBASE_PRIVATE_KEY}",
-  //   "encryption_key": "${process.env.ENCRYPTION_KEY}"
-  // }
-  // `;
+  contents = `
+  {
+    "env": "${process.env.NODE_ENV}", 
+    "mongoUri": "${process.env.DATABASE_URI}",
+    "mongoUsername": "${process.env.DATABASE_USERNAME}", 
+    "mongoPassword": "${process.env.DATABASE_PASSWORD}", 
+    "database": "${process.env.DATABASE}", 
+    "jwt_secret_key": "${process.env.JWT_SECRET_KEY}", 
+    "jwt_refresh_secret_key": "${process.env.JWT_REFRESH_SECRET_KEY}",
+    "firebase_project_Id": "${process.env.FIREBASE_PROJECT_ID}",
+    "firebase_client_email": "${process.env.FIREBASE_CLIENT_EMAIL}",
+    "firebase_private_key": "${process.env.FIREBASE_PRIVATE_KEY}",
+    "encryption_key": "${process.env.ENCRYPTION_KEY}"
+  }
+  `;
   env = JSON.parse(contents.toString());
 
   return env;
