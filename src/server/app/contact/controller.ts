1a0bb9d112b7d4a0e0c27fc8c0e85d3e43649471
YazanWellx1 <162117897+YazanWellx1@users.noreply.github.com>
2024-05-23 15:58:09 +0400

    fix contact object

--- src/server/app/contact/controller.ts
+++ src/server/app/contact/controller.ts
@@ -223,10 +223,9 @@ class ContactsControllers {
       const hashedPassword = await hashPassword(password);
       console.log('hashedPassword', hashedPassword);
       console.log('password', password);
-      const data = await service.contactService.updateContact(contactId, {
-        ...existingContact,
-        password: hashedPassword,
-      });
+      const newContact = Object.assign({...existingContact}, {password: hashedPassword})
+      console.log('assigned contact', newContact)
+      const data = await service.contactService.updateContact(contactId, newContact);
       console.log('data', data)
 
       let email = `<p>Liebe Kund*innen,</p><br>Ihr Konto wurde erfolgreich aktualisiert. Wir empfehlen Ihnen, sich auf der <a href='https://bgas-kalender.at/login'>Website</a> mit den folgenden Anmeldeinformationen anzumelden und aus Sicherheitsgründen Ihr Passwort zu ändern:<br>E-Mail: ${existingContact.email}<br>Passwort: ${password}<br><p>Vielen Dank, dass Sie unsere Dienste gewählt haben.</p><p>Mit freundlichen Grüßen,</p><img src='https://firebasestorage.googleapis.com/v0/b/b-gas-13308.appspot.com/o/bgas-logo.png?alt=media&token=7ebf87ca-c995-4266-b660-a4c354460ace' alt='Company Signature Logo' width='150'>`
