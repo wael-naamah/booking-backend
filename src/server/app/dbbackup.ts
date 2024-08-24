f85e77a62bbf865c0eb32e951a402a51202512cc
wael-naamah <wael@fuze.us>
2024-08-24 20:15:06 +0400

    fix db backup

--- src/server/app/dbbackup.ts
+++ src/server/app/dbbackup.ts
@@ -5,24 +5,21 @@ import { getEnv } from "../env";
 // Function to perform database backup
 export const backupDatabase = async () => {
   console.info("Backup started...");
-  const sourceDb = getEnv().database;
-  const targetDb = getEnv().database_kalendar;
 
-  // Exclude archived contacts and appointments
-  const excludeArchivedContacts =
-    "--excludeCollection=contacts --excludeCollectionWithPrefix=contacts.archived";
-  const excludeArchivedAppointments =
-    "--excludeCollection=appointments --excludeCollectionWithPrefix=appointments.archived";
+  // Exclude archived contacts and appointments collections
+  const excludeArchivedContacts = "--excludeCollection=contacts.archived";
+  const excludeArchivedAppointments = "--excludeCollection=appointments.archived";
 
   const backupCommand = `mongodump --uri="mongodb://${getEnv().mongoUsername}:${
     getEnv().mongoPassword
   }@${getEnv().mongoHost}:${getEnv().mongoPort}/${
     getEnv().database
-  }?directconnection=true&authSource=admin&replicaSet=replicaset&retryWrites=true" --db=${sourceDb} ${excludeArchivedContacts} ${excludeArchivedAppointments} --archive | mongorestore --uri="mongodb://${
+  }?directconnection=true&authSource=admin&replicaSet=replicaset&retryWrites=true" ${excludeArchivedContacts} ${excludeArchivedAppointments} --archive | mongorestore --uri="mongodb://${
     getEnv().mongoUsername
   }:${getEnv().mongoPassword}@${getEnv().mongoHost}:${getEnv().mongoPort}/${
-    getEnv().database
-  }?directconnection=true&authSource=admin&replicaSet=replicaset&retryWrites=true" --db=${targetDb} --archive --drop`;
+    getEnv().database_kalendar
+  }?directconnection=true&authSource=admin&replicaSet=replicaset&retryWrites=true" --archive --drop`;
+
   console.log(backupCommand);
 
   exec(backupCommand, (error, stdout, stderr) => {
@@ -40,8 +37,6 @@ export const backupDatabase = async () => {
 };
 
 // Schedule the backup job to run every day at midnight
-
-
 export default function dbbackup() {
   cron.schedule("0 0 * * *", () => {
     console.log("Running daily database backup...");
